c82830d01cac9ba4d047c6d0b9fd006401e43852
Markus Enzweiler <markus.enzweiler@hs-esslingen.de>
2023-12-26 18:40:28 +0100

    removed debug output

--- loss.py
+++ loss.py
@@ -12,6 +12,6 @@ def mse_kl_loss(x, x_recon, kl_div, alpha_kl_div_loss=1.0):
 
     # Total loss
     loss = recon_loss + alpha_kl_div_loss * kl_div
-    print(f"Loss: {loss.item():.2f} | Recon loss: {recon_loss.item():.2f} | KL div: {kl_div.item():.2f}", end="\n", flush=True)
+    #print(f"Loss: {loss.item():.2f} | Recon loss: {recon_loss.item():.2f} | KL div: {kl_div.item():.2f}", end="\n", flush=True)
     return loss
 
